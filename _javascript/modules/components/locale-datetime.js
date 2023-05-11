229df2b4ef506354ae154846cc21746080e6465c
Shih-Ting, Yuan <shihtiy.tw@gmail.com>
2023-05-11 23:33:14 +0000

    fix git confliction

--- _javascript/modules/components/locale-datetime.js
+++ _javascript/modules/components/locale-datetime.js
@@ -5,44 +5,6 @@
  */
 
 /* A tool for locale datetime */
-<<<<<<< HEAD:_javascript/utils/locale-datetime.js
-const LocaleHelper = (function () {
-    const locale = $('html').attr('lang').substr(0, 2);
-    const attrTimestamp = 'data-ts';
-    const attrDateFormat = 'data-df';
-
-    return {
-        locale: () => locale,
-        attrTimestamp: () => attrTimestamp,
-        attrDateFormat: () => attrDateFormat,
-        getTimestamp: ($elem) => Number($elem.attr(attrTimestamp)), // unix timestamp
-        getDateFormat: ($elem) => $elem.attr(attrDateFormat)
-    };
-
-}());
-
-$(function () {
-    dayjs.locale(LocaleHelper.locale());
-    dayjs.extend(window.dayjs_plugin_localizedFormat);
-
-    $(`[${LocaleHelper.attrTimestamp()}]`).each(function () {
-        const date = dayjs.unix(LocaleHelper.getTimestamp($(this)));
-        const text = date.format(LocaleHelper.getDateFormat($(this)));
-        $(this).text(text);
-        $(this).removeAttr(LocaleHelper.attrTimestamp());
-        $(this).removeAttr(LocaleHelper.attrDateFormat());
-
-        // setup tooltips
-        const tooltip = $(this).attr('data-toggle');
-        if (typeof tooltip === 'undefined' || tooltip !== 'tooltip') {
-            return;
-        }
-
-        const tooltipText = date.format('llll'); // see: https://day.js.org/docs/en/display/format#list-of-localized-formats
-        $(this).attr('data-original-title', tooltipText);
-    });
-});
-=======
 class LocaleHelper {
   static get attrTimestamp() {
     return 'data-ts';
@@ -86,4 +48,3 @@ export function initLocaleDatetime() {
     $(this).attr('data-original-title', tooltipText);
   });
 }
->>>>>>> upstream:_javascript/modules/components/locale-datetime.js
--- _javascript/modules/components/pageviews.js
+++ _javascript/modules/components/pageviews.js
@@ -7,122 +7,6 @@
  */
 
 const getInitStatus = (function () {
-<<<<<<< HEAD:_javascript/utils/pageviews.js
-    let hasInit = false;
-    return () => {
-        let ret = hasInit;
-        if (!hasInit) {
-            hasInit = true;
-        }
-        return ret;
-    };
-}());
-
-const PvOpts = (function () {
-    function getContent(selector) {
-        return $(selector).attr("content");
-    }
-
-    function hasContent(selector) {
-        let content = getContent(selector);
-        return (typeof content !== "undefined" && content !== false);
-    }
-
-    return {
-        getProxyMeta() {
-            return getContent("meta[name=pv-proxy-endpoint]");
-        },
-        getLocalMeta() {
-            return getContent("meta[name=pv-cache-path]");
-        },
-        hasProxyMeta() {
-            return hasContent("meta[name=pv-proxy-endpoint]");
-        },
-        hasLocalMeta() {
-            return hasContent("meta[name=pv-cache-path]");
-        }
-    };
-
-}());
-
-const PvStorage = (function () {
-    const Keys = {
-        KEY_PV: "pv",
-        KEY_PV_SRC: "pv_src",
-        KEY_CREATION: "pv_created_date"
-    };
-
-    const Source = {
-        LOCAL: "same-origin",
-        PROXY: "cors"
-    };
-
-    function get(key) {
-        return localStorage.getItem(key);
-    }
-
-    function set(key, val) {
-        localStorage.setItem(key, val);
-    }
-
-    function saveCache(pv, src) {
-        set(Keys.KEY_PV, pv);
-        set(Keys.KEY_PV_SRC, src);
-        set(Keys.KEY_CREATION, new Date().toJSON());
-    }
-
-    return {
-        keysCount() {
-            return Object.keys(Keys).length;
-        },
-        hasCache() {
-            return (localStorage.getItem(Keys.KEY_PV) !== null);
-        },
-        getCache() {
-            return JSON.parse(localStorage.getItem(Keys.KEY_PV));
-        },
-        saveLocalCache(pv) {
-            saveCache(pv, Source.LOCAL);
-        },
-        saveProxyCache(pv) {
-            saveCache(pv, Source.PROXY);
-        },
-        isExpired() {
-            let date = new Date(get(Keys.KEY_CREATION));
-            date.setHours(date.getHours() + 1);   // per hour
-            return Date.now() >= date.getTime();
-        },
-        isFromLocal() {
-            return get(Keys.KEY_PV_SRC) === Source.LOCAL;
-        },
-        isFromProxy() {
-            return get(Keys.KEY_PV_SRC) === Source.PROXY;
-        },
-        newerThan(pv) {
-            return PvStorage.getCache().totalsForAllResults["ga:pageviews"] > pv.totalsForAllResults["ga:pageviews"];
-        },
-        inspectKeys() {
-            if (localStorage.length !== PvStorage.keysCount()) {
-                localStorage.clear();
-                return;
-            }
-
-            for (let i = 0; i < localStorage.length; i++) {
-                const key = localStorage.key(i);
-                switch (key) {
-                    case Keys.KEY_PV:
-                    case Keys.KEY_PV_SRC:
-                    case Keys.KEY_CREATION:
-                        break;
-                    default:
-                        localStorage.clear();
-                        return;
-                }
-            }
-        }
-    };
-}()); /* PvStorage */
-=======
   let hasInit = false;
   return () => {
     let ret = hasInit;
@@ -239,7 +123,6 @@ const PvStorage = (function () {
     }
   };
 })(); /* PvStorage */
->>>>>>> upstream:_javascript/modules/components/pageviews.js
 
 function countUp(min, max, destId) {
     if (min < max) {
@@ -255,16 +138,6 @@ function countUp(min, max, destId) {
 function countPV(path, rows) {
     let count = 0;
 
-<<<<<<< HEAD:_javascript/utils/pageviews.js
-    if (typeof rows !== "undefined") {
-        for (let i = 0; i < rows.length; ++i) {
-            const gaPath = rows[parseInt(i, 10)][0];
-            if (gaPath === path) { /* path format see: site.permalink */
-                count += parseInt(rows[parseInt(i, 10)][1], 10);
-                break;
-            }
-        }
-=======
   if (typeof rows !== 'undefined') {
     for (let i = 0; i < rows.length; ++i) {
       const gaPath = rows[parseInt(i, 10)][0];
@@ -273,25 +146,12 @@ function countPV(path, rows) {
         count += parseInt(rows[parseInt(i, 10)][1], 10);
         break;
       }
->>>>>>> upstream:_javascript/modules/components/pageviews.js
     }
 
     return count;
 }
 
 function tacklePV(rows, path, elem, hasInit) {
-<<<<<<< HEAD:_javascript/utils/pageviews.js
-    let count = countPV(path, rows);
-    count = (count === 0 ? 1 : count);
-
-    if (!hasInit) {
-        elem.text(new Intl.NumberFormat().format(count));
-    } else {
-        const initCount = parseInt(elem.text().replace(/,/g, ""), 10);
-        if (count > initCount) {
-            countUp(initCount, count, elem.attr("id"));
-        }
-=======
   let count = countPV(path, rows);
   count = count === 0 ? 1 : count;
 
@@ -301,76 +161,17 @@ function tacklePV(rows, path, elem, hasInit) {
     const initCount = parseInt(elem.text().replace(/,/g, ''), 10);
     if (count > initCount) {
       countUp(initCount, count, elem.attr('id'));
->>>>>>> upstream:_javascript/modules/components/pageviews.js
     }
 }
 
 function displayPageviews(data) {
-<<<<<<< HEAD:_javascript/utils/pageviews.js
-    if (typeof data === "undefined") {
-        return;
-    }
-=======
   if (typeof data === 'undefined') {
     return;
   }
->>>>>>> upstream:_javascript/modules/components/pageviews.js
 
     let hasInit = getInitStatus();
     const rows = data.rows; /* could be undefined */
 
-<<<<<<< HEAD:_javascript/utils/pageviews.js
-    if ($("#post-list").length > 0) { /* the Home page */
-        $(".post-preview").each(function () {
-            const path = $(this).find("a").attr("href");
-            tacklePV(rows, path, $(this).find(".pageviews"), hasInit);
-        });
-
-    } else if ($(".post").length > 0) { /* the post */
-        const path = window.location.pathname;
-        tacklePV(rows, path, $("#pv"), hasInit);
-    }
-}
-
-function fetchProxyPageviews() {
-    if (PvOpts.hasProxyMeta()) {
-        $.ajax({
-            type: "GET",
-            url: PvOpts.getProxyMeta(),
-            dataType: "jsonp",
-            jsonpCallback: "displayPageviews",
-            success: (data) => {
-                PvStorage.saveProxyCache(JSON.stringify(data));
-            },
-            error: (jqXHR, textStatus, errorThrown) => {
-                console.log("Failed to load pageviews from proxy server: " + errorThrown);
-            }
-        });
-    }
-}
-
-function fetchLocalPageviews(hasCache = false) {
-    return fetch(PvOpts.getLocalMeta())
-        .then(response => response.json())
-        .then(data => {
-            if (hasCache) {
-                // The cache from the proxy will sometimes be more recent than the local one
-                if (PvStorage.isFromProxy() && PvStorage.newerThan(data)) {
-                    return;
-                }
-            }
-            displayPageviews(data);
-            PvStorage.saveLocalCache(JSON.stringify(data));
-        });
-}
-
-$(function () {
-    if ($(".pageviews").length <= 0) {
-        return;
-    }
-
-    PvStorage.inspectKeys();
-=======
   if ($('#post-list').length > 0) {
     /* the Home page */
     $('.post-preview').each(function () {
@@ -441,7 +242,6 @@ export function initPageviews() {
     }
   } else {
     // no cached
->>>>>>> upstream:_javascript/modules/components/pageviews.js
 
     if (PvStorage.hasCache()) {
         displayPageviews(PvStorage.getCache());
@@ -467,10 +267,5 @@ export function initPageviews() {
             fetchProxyPageviews();
         }
     }
-<<<<<<< HEAD:_javascript/utils/pageviews.js
-
-});
-=======
   }
 }
->>>>>>> upstream:_javascript/modules/components/pageviews.js
